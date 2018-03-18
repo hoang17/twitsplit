4857b2bed7f001146a3ec9a2ca829e94149f7465
Hoang Le <lehuyhoang117@gmail.com>
2018-03-19 02:28:38 +0700

    no message

--- actions/runtime.js
+++
@@ -1,8 +0,0 @@
-import { SET_RUNTIME_VARIABLE } from '../constants'
-
-export function setRuntimeVariable(name, value) {
-  return {
-    type: SET_RUNTIME_VARIABLE,
-    payload: { name, value },
-  }
-}
--- reducers/index.js
+++ reducers/index.js
@@ -1,23 +1,13 @@
 import { combineReducers } from 'redux'
 
-// import runtime from './runtime'
-// import hello from './hello'
 // import jsonAPI from './jsonAPI'
-// import next from './next'
-// import todos from './todos'
-// import visibilityFilter from './visibilityFilter'
 
 import event from './event'
 import question from './question'
 
 export const makeRootReducer = (asyncReducers) => {
   return combineReducers({
-    // runtime,
     // jsonAPI,
-    // hello,
-    // next,
-    // todos,
-    // visibilityFilter,
     event,
     question,
     ...asyncReducers
